d2989422e3b4bc271393fadab62e45f984f9dd7d
Anna Schmidt <marshall1697@gmail.com>
2022-10-12 11:25:28 -0600

    commit tree c78bf97391045c1fe7ff0e6dd07c7559371437ff

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
 require 'pry'
 
+
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    single_sentences = self.split(/[.?!]/).filter do |sentence|
+      if !sentence.empty?
+        sentence
+      end
+    end
+    single_sentences.count
   end
+  
+  "hello there. I love cookies!".count_sentences
 end
